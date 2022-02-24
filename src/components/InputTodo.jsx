c93c5ebbc3da7616556dcb5d7fa77c8800bb9152
s-machino <hamu3103hamu@gmail.com>
2022-02-24 21:41:20 +0900

    add new file

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  backgroundColor: '#ccc',
+  height: '30px',
+};
+
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
-    <div className="input-area">
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+    <div style={style} className="input-area">
+      <input disabled={disabled} placeholder="TODOを入力" value={todoText} onChange={onChange} />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
